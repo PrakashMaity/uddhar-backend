fc0fc32dce836dccec9a0e7a4ab33a708aea7eca
Tamojit <tamojit.biswas@sentientgeeks.com>
2023-03-29 19:06:46 +0530

    code changes done

--- src/validation/validationMiddleware.ts
+++ src/validation/validationMiddleware.ts
@@ -1,10 +1,13 @@
-const validate = (schema: any) => (req: any, res: any, next: any) => {
-  const { error } = schema.validate(req.body);
-  if (error) {
-    res.status(422).send(error.details[0].message);
-  } else {
-    next();
-  }
-};
+import { NextFunction, Request, Response } from "express";
+
+const validate =
+  (schema: any) => (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      res.status(422).send(error.details[0].message);
+    } else {
+      next();
+    }
+  };
 
 export default validate;
